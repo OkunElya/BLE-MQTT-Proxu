ad7f216386e97d022e3b1f02061ae8971b7eecf3
OkunElya <okun.elya@gmail.com>
2026-02-04 13:58:41 +1000

    addded formats extension

--- extensions/format.py
+++ extensions/format.py
@@ -1,12 +1,44 @@
 import struct
 
+class pack:
+    @staticmethod
+    def int16l(val):
+        return struct.pack("<h", val)
 
-def int16l(val):
-    return struct.pack("<h", val)
+    @staticmethod
+    def int32l(val):
+        return struct.pack("<i", val)
 
-def float16l(val):
-    return struct.pack("<e", val)
+    @staticmethod
+    def float32l(val):
+        return struct.pack("<f", val)
+    
+    @staticmethod
+    def float16l(val):
+        return struct.pack("<e", val)
 
-def byte(val):
-    val=min(255,max(0,int(val)))
-    return bytes(val)
+    @staticmethod
+    def byte(val):
+        val=min(255,max(0,int(val)))
+        return bytes(val)
+
+class unpack:
+    @staticmethod
+    def int16l(data):
+        return struct.unpack("<h", data)[0]
+
+    @staticmethod
+    def int32l(data):
+        return struct.unpack("<i", data)[0]
+
+    @staticmethod
+    def float32l(data):
+        return struct.unpack("<f", data)[0]
+
+    @staticmethod
+    def float16l(data):
+        return struct.unpack("<e", data)[0]
+
+    @staticmethod
+    def byte(data):
+        return data[0]
