33202715a6be9433bfdee66eba31fb938ebc327e
OkunElya <okun.elya@gmail.com>
2026-02-05 11:01:59 +1000

    base for mqtt topic publishing created

---
+++ mqtt_connector.py
@@ -0,0 +1,249 @@
+import asyncio
+import aiomqtt
+import json
+import random
+import time
+from typing import Any
+from typing import Optional, Callable
+import logging
+
+
+class TopicPayload:
+    def __init__(self, config,local_context:dict[str, Any]):
+        self.local_context = local_context
+        self.config = config
+        self.get_populated()
+        
+    def get_populated(self):
+        return  self.recursive_replace(self.config)
+    
+    def replace_str(self, string:str):
+        try:
+            return eval(string, self.local_context, self.local_context)
+        except Exception as e:
+            raise ValueError(f"Failed to replace '{string}': {e}")
+
+    def recursive_replace(self, config_part):
+        if isinstance(config_part, dict):
+            buffer_obj = {}
+            for key, value in config_part.items():
+                buffer_obj[self.recursive_replace(key)] = self.recursive_replace(value)
+            return buffer_obj
+        elif isinstance(config_part, list):
+            buffer_obj = []
+            for item in config_part:
+                buffer_obj.append(self.recursive_replace(item))
+            return buffer_obj
+        elif isinstance(config_part, str):
+            return self.replace_str(config_part)
+        else:
+            return config_part
+
+class TopicTrigger:
+    delay: Optional[float]
+    interval: Optional[float]
+    condition: Optional[Callable[[], bool]]
+    last_interval_trigger: float
+    last_delay_trigger: float
+    
+    _parent_link: "TopicTriggerCollection"
+    
+    
+    def __init__(self, config:dict,local_context:dict[str, Any],parent_link: "TopicTriggerCollection",logger:logging.Logger):
+        self.logger = logger
+        self._parent_link = parent_link
+        if not isinstance(config, dict):
+            raise TypeError("Config for sendOn must be a dict")
+        self.config = config
+        
+        self.local_context = local_context
+        
+        self.delay = None
+        self.interval = None
+        self.condition = None
+        self.last_interval_trigger = 0
+        self.last_delay_trigger = 0
+        
+        self.prepare()
+    
+    def prepare(self):
+        for key, value in self.config.items():
+            if key == "equation":
+                #try to eval 
+                # self.config[key] =  self.config[key]
+                try:
+                    
+                    self.condition =lambda : eval(self.config["equation"], self.local_context, self.local_context)
+                    self.condition()
+                except Exception as e:
+                    raise ValueError(f"Failed to eval '{self.config[key]}': {e}")
+                # Test the condition to ensure it returns a bool
+                try:
+                    result = self.condition()
+                    if not isinstance(result, bool):
+                        raise ValueError("equation condition must return a boolean value")
+                except Exception as e:
+                    raise ValueError(f"Failed to execute equation condition: {e}")
+            if key == "interval":
+                try:
+                    float(value)
+                except (TypeError, ValueError):
+                    raise ValueError(f"Interval value '{value}' is not convertible to float")
+                self.last_interval_trigger = time.monotonic() - float(value) 
+                self.interval = float(value)
+            if key == "delay":
+                try:
+                    float(value)
+                except (TypeError, ValueError):
+                    raise ValueError(f"Interval value '{value}' is not convertible to float")
+                self.last_delay_trigger = time.monotonic() - float(value) 
+                self.delay = float(value)
+        self.get_query_interval()
+        
+        
+                
+    async def check(self):
+        #delay is first priority, interval second and executable condition is last
+        if self.delay is not None:
+            now = time.monotonic()
+            if now - self.last_delay_trigger < self.delay:
+                return False
+            self.last_delay_trigger = now
+
+        if self.interval is not None:
+            now = time.monotonic()
+            if now - self.last_interval_trigger < self.interval:
+                return False
+            self.last_interval_trigger = now
+
+        if  self.condition is not None:
+            try:
+                return bool(self.condition())
+            except Exception as e:
+                raise ValueError(f"Failed to execute toEval: {e}")
+        
+        return True
+
+    def get_query_interval(self) -> float:
+        min_interval = None
+        if self.delay is not None:
+            min_interval = self.delay
+        if self.interval is not None:
+            if min_interval is None:
+                min_interval = self.interval
+            else:
+                min_interval = min(min_interval,self.interval)
+        
+        if min_interval is None:
+            min_interval = 1.0 #default to sensible delay 
+        return min_interval
+    
+    async def query_loop(self):
+        interval = self.get_query_interval()
+        while interval is not None:
+            if await self.check():
+                try:
+                    await self._parent_link._parent_link.send()
+                except Exception as e:
+                    self.logger.error(f"Failed to post message: {e} | Trigger config: {self.config}")
+            await asyncio.sleep(interval)
+
+
+class TopicTriggerCollection:
+    post_message_callback: lambda x: None = None
+    trigger_list: list[TopicTrigger] = []
+    trigger_futures_list: list[asyncio.Task] = []
+
+    _parent_link: "OutputTopic"
+   
+    def __init__(self,config:list,local_context, _parent_link:"OutputTopic",logger:logging.Logger):
+        self.logger = logger
+        self._parent_link = _parent_link
+        self.config = config
+        self.local_context = local_context
+        
+        if not isinstance(config, list):
+            raise TypeError("Config for TopicTriggerCollection must be a list")
+
+        for trigger_config in config:
+            trigger = TopicTrigger(trigger_config, local_context,self,self.logger)
+            self.trigger_list.append(trigger)
+            self.trigger_futures_list.append(trigger.query_loop())
+            
+
+class OutputTopic:
+    name: str
+    logger: logging.Logger
+    payload: TopicPayload
+    trigger_collection: TopicTriggerCollection
+    
+    
+    def __init__(self, config:dict[str, Any], name:str,client:aiomqtt.Client, local_context:dict [str, Any], logger:logging.Logger):
+        self.logger = logger
+        self.local_context = local_context
+        self.client = client
+        
+        if "payload" not in config or "sendOn" not in config:
+            raise KeyError("Both 'payload' and 'sendOn' keys must be present in the config")
+        self.config = config
+        self.name = name
+        self.payload = TopicPayload(config["payload"],self.local_context)
+        self.trigger_collection = TopicTriggerCollection(config["sendOn"],self.local_context,self,self.logger)
+
+
+    async def send(self):
+        payload = self.payload.get_populated()
+        if isinstance(payload, (dict, list)):
+            payload = json.dumps(payload)
+        await self.client.publish(self.name,payload)
+        
+    def get_corutines(self):
+        return self.trigger_collection.trigger_futures_list
+    
+    
+
+
+        
+        
+if __name__ == "__main__":
+    from device import DeviceCollection
+    with open("./config.json", "r") as F:
+        device_config = json.load(F)["devices"]
+    Devices = DeviceCollection(device_config)
+    with open("config.json") as F:
+        mqtt_config = json.load(F)["mqtt"]
+    
+    
+    context = {}
+    logger = logging.getLogger()
+    client_id = f"BLE proxy-{''.join(random.choices('abcdefghijklmnopqrstuvwxyz0123456789', k=4))}"
+    server_address = mqtt_config.get("serverAddress", None)
+
+    if server_address is None:
+        raise ValueError("Missing server address in config.json")
+
+    server_hostname = server_address.split(":")[0]
+    server_port = 1883
+    if ":" in server_address:
+        server_port = int(server_address.split(":")[-1])
+
+    username = mqtt_config.get("username", None)
+    password = mqtt_config.get("password", None)
+    
+    
+    async def main():
+        async with aiomqtt.Client(hostname=server_hostname, port=server_port,username=username,password=password,identifier=client_id) as client:
+            topic_list = []
+            all_tasks = []
+            for topic_name, topic_config in mqtt_config["topics"]["toSendTo"].items():
+                topic_obj = OutputTopic(topic_config,topic_name,client, {**locals(),**globals()},logger)
+                topic_list.append(topic_obj)
+                all_tasks = topic_obj.get_corutines()
+            all_tasks += DeviceCollection.devices_corutines
+            await asyncio.gather(*(task for task in all_tasks))
+
+
+    
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(asyncio.gather(main()))
